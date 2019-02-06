6dc1358e68563b3c0607a876de00dcb65b11c7cc
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-06 11:31:22 +0100

    Start setup passport strategy

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
+passport.use(new GoogleStrategy());
 
-app.get('/', (req, res) => {
-  res.send({bye: 'buddy'})
-});
+app.use();
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
+
+
+// http://localhost:5000/auth/google/callback
