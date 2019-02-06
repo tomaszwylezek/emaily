083c813855f94965a225c68189a487d3067e8e67
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-06 12:25:51 +0100

    Refactor and add use of passport

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-
+require("./services/passport");
 const app = express();
 
-passport.use(new GoogleStrategy());
-
-app.use();
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-
-// http://localhost:5000/auth/google/callback
+module.exports = {
+  app
+};
