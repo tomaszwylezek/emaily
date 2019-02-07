a0d4e474cc30b425518da506ba188afc93570df7
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-07 13:52:39 +0100

    Add Stripe to node and add body-parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const keys = require("./config/keys");
 
@@ -15,6 +16,7 @@ mongoose.connect(
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -25,6 +27,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
