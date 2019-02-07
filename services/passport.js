855199e530ea8815b02b1583f88af0aef2efeb82
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-07 10:44:07 +0100

    Setupping react (setup redux)

--- services/passport.js
+++ services/passport.js
@@ -28,14 +28,14 @@ passport.use(
       const existingUser = await User.findOne({ googleId: profile.id });
 
       if (existingUser) {
-        done(null, existingUser);
-      } else {
-        const user = await new User({
-          googleId: profile.id
-        }).save();
-
-        done(null, user);
+        return done(null, existingUser);
       }
+
+      const user = await new User({
+        googleId: profile.id
+      }).save();
+
+      done(null, user);
     }
   )
 );
