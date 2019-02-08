df419a0ec7eb0b2f760e57fffda8b6be045674f7
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-08 14:41:41 +0100

    Complete action creator and add redirect

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,9 +1,12 @@
 import React from "react";
 import _ from "lodash";
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+
+import { submitSurvey } from "../../actions";
 import formFields from "./formFields";
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(formFields, field => (
     <div key={field.name}>
       <label>{field.label}</label>
@@ -14,9 +17,16 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <div>{reviewFields()}</div>
+      <div>{reviewFields}</div>
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
+      </button>
+      <button
+        className="green btn-flat right white-text"
+        onClick={() => submitSurvey(formValues, history)}
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
@@ -26,4 +36,7 @@ const mapStateToProps = ({ form }) => ({
   formValues: form.surveyForm.values
 });
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(
+  mapStateToProps,
+  { submitSurvey }
+)(withRouter(SurveyFormReview));
