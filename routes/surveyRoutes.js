744209ee3c2146de9f8d0e3b96ef86999073a7da
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-08 10:02:54 +0100

    Setup SendGrid kkeys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -5,16 +5,15 @@ const requireCredits = require("../middlewarers/requireCredits");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
       title,
       subject,
       body,
-      recipients: recipients.split(",").map(email => ({
-        email
-      })),
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+
       _user: req.user.id,
       dateSent: Date().now()
     });
