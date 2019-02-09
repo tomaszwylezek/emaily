f044f819cde065f0131da5649c36266f168eed23
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-09 15:53:44 +0100

    Start adding webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,11 @@
+const _ = require("lodash");
 const mongoose = require("mongoose");
+const { Path } = require("path-parser");
+const { URL } = require("url");
+
 const requireLogin = require("../middlewarers/requireLogin");
 const requireCredits = require("../middlewarers/requireCredits");
+
 const Mailer = require("../services/Mailer");
 const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
@@ -9,6 +14,23 @@ const Survey = mongoose.model("surveys");
 module.exports = app => {
   app.get("/api/surveys/thanks", (req, res) => {
     res.send("Thanks for voting!");
+  });
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const p = new Path("/api/surveys/:surveyId/:choice");
+
+    const events = _.chain(req.body)
+      .map(({ url, email }) => {
+        const match = p.test(new URL(url).pathname);
+
+        if (match) {
+          return { email, ...match };
+        }
+      })
+      .compact()
+      .uniqBy(({ email, surveyId }) => email + surveyId);
+
+    res.send({});
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
